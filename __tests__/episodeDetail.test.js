cd0d6aa483cad39a40ba9651c011519f386a996c
Ana Gil Amor <kairos@MBP-de-Ana.home>
2023-04-17 15:49:53 +0200

    feat((test)): add episode page tests Add episode page tests

---
+++ __tests__/episodeDetail.test.js
@@ -0,0 +1,52 @@
+import useGetEpisode from '@/hooks/use-get-episode'
+import episodeDummy from '@/mocks/episodeDummy'
+import podcastDummy from '@/mocks/podcastDummy'
+import PodcastPage from '@/pages/podcast/[id]'
+import EpisodePage from '@/pages/podcast/[id]/episode/[episodeId]'
+import { render, screen } from '@testing-library/react'
+
+jest.mock('../hooks/use-get-episode.js')
+
+describe('EpisodePage', () => {
+  it('renders the podcast title', async () => {
+    const podcastId = '1535809341'
+    const episodeId = '1000609059022'
+    const data = episodeDummy
+
+    useGetEpisode.mockImplementation(() => data)
+
+    render(<EpisodePage episodeId={episodeId} podcastId={podcastId} />)
+
+    const heading = screen.getByRole('heading', {
+      name: 'The Joe Budden Podcast',
+    })
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders the image', async () => {
+    const podcastId = '1535809341'
+    const episodeId = '1000609059022'
+    const data = episodeDummy
+
+    useGetEpisode.mockImplementation(() => data)
+
+    render(<EpisodePage episodeId={episodeId} podcastId={podcastId} />)
+
+    const image = screen.getByAltText('Icon for podcast The Joe Budden Podcast')
+    expect(image).toBeInTheDocument()
+  })
+  it('renders the episode title', async () => {
+    const podcastId = '1535809341'
+    const episodeId = '1000609059022'
+    const data = episodeDummy
+
+    useGetEpisode.mockImplementation(() => data)
+
+    render(<EpisodePage episodeId={episodeId} podcastId={podcastId} />)
+
+    const heading = screen.getByRole('heading', {
+      name: 'Episode 618 | "They Got Us"',
+    })
+    expect(heading).toBeInTheDocument()
+  })
+})
